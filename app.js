3ca0412e9baa65ba406f47ed31240d00d53cb090
Toru <pub@otchy.com>
2020-03-19 15:59:43 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('The app is running!'));
+app.get('/', (req, res) => res.send('The app is running on Heroku!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
